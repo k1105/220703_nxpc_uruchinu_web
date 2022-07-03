c01b0d334437840d3b64d76a00b93c5d92324326
k1105 <kanapon1105@yahoo.co.jp>
2022-07-03 20:48:12 +0900

    thumbnailContainerの追加

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+import { ThumbnailContainer } from "./lib/ThumbnailContainer";
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div style={{ width: `90%`, margin: "30px auto" }}>
+        <header>
+          <Notification>
+            If you want to see more porn videos in 日本語, you can simply upload
+            some and write their title in 日本語! This index is updated every 30
+            minutes.
+          </Notification>
+        </header>
+        <div style={{ textAlign: "center" }}>
+          {(() => {
+            const thumbnails = [];
+            for (let i = 0; i < 20; i++) {
+              thumbnails.push(
+                <Container>
+                  <ThumbnailContainer num={i} />
+                </Container>
+              );
+            }
+            return <div>{thumbnails}</div>;
+          })()}
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+const Container = styled.div`
+  display: inline-block;
+  margin: 50px 10px;
+`;
+
+const Notification = styled.div`
+  background: #ccc;
+`;
---
+++ src/lib/ThumbnailContainer.js
@@ -0,0 +1,69 @@
+import styled from "styled-components";
+
+export const ThumbnailContainer = (props) => {
+  console.log(props.num);
+  return (
+    <div>
+      <Container>
+        <Thumbnail src={`${process.env.PUBLIC_URL}/thumbnail/1.jpg`} />
+        <Quality>1080p</Quality>
+        <Title>{`汗だく！汁だく！その${props.num}`}</Title>
+        <Information>
+          <Uploader>uploader</Uploader>
+          <Viewer>2.1M</Viewer>
+          <Time>6 min </Time>
+        </Information>
+      </Container>
+    </div>
+  );
+};
+
+const Container = styled.div`
+  position: relative;
+  height: 180px;
+  width: 320px;
+`;
+
+const Thumbnail = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const Quality = styled.div`
+  box-sizing: border-box;
+  position: absolute;
+  content: "";
+  top: 10px;
+  right: 10px;
+  background: #ff0000;
+  color: white;
+  padding: 2px;
+  border-radius: 2px;
+`;
+
+const Title = styled.div`
+  text-align: center;
+`;
+
+const Information = styled.div`
+  text-align: center;
+`;
+
+const Viewer = styled.div`
+  display: inline-block;
+  margin-right: 10px;
+`;
+
+const Uploader = styled.div`
+  display: inline-block;
+  margin-right: 10px;
+`;
+
+const Time = styled.div`
+  display: inline-block;
+  font-size: 0.7rem;
+  background: #999;
+  color: white;
+  padding: 2px;
+  border-radius: 2px;
+`;
